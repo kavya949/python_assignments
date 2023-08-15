a39db1624ffda303c22e8fc1e040bafe6d4a05f8
kavya949 <142312499+kavya949@users.noreply.github.com>
2023-08-15 17:02:56 +0530

    Create area_of_circle.py
    the file prints area of the circle on the screen.

---
+++ area_of_circle.py
@@ -0,0 +1,4 @@
+print("Input the raddius of the circle : ")
+radius=float(input()) #taking input
+area=(22*radius**2)/7 #area of the circle formula
+print("The area of the circle with radius ",radius," is : ",area)
